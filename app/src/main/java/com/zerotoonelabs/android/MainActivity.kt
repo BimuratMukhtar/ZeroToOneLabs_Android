14b51894beb4cd5b16525f02b86c6d2430ba819b
BimuratMukhtar <bimurat.mukhtar@gmail.com>
2018-12-17 21:09:26 +0600

    initial commit

---
+++ app/src/main/java/com/zerotoonelabs/android/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.zerotoonelabs.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.zerotoonelabs.android.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/zerotoonelabs/android/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.zerotoonelabs.android.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
