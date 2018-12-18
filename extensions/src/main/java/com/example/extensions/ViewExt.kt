b516714953536083cc6c6c66df27f43b18471fc2
lelesy <beis96a@gmail.com>
2018-12-18 12:14:11 +0600

    feat: view extensions

---
+++ extensions/src/main/java/com/example/extensions/ViewExt.kt
@@ -0,0 +1,91 @@
+package com.example.extensions
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import javax.xml.datatype.DatatypeConstants.DURATION
+
+//provide simpler access to getString(int)
+fun View.getString(stringResId: Int): String = resources.getString(stringResId)
+
+
+//method to show a keyboard for View
+fun View.showKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    this.requestFocus()
+    imm.showSoftInput(this, 0)
+}
+
+
+//to hide the keyboard and returns whether it worked
+fun View.hideKeyboard(): Boolean {
+    try {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        return inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+    } catch (ignored: RuntimeException) { }
+    return false
+}
+
+//to provide quicker access to the LayoutInflater from a View
+fun View.getLayoutInflater() = context.getLayoutInflater()
+
+//to set View's left padding
+fun View.setPaddingLeft(value: Int) = setPadding(value, paddingTop, paddingRight, paddingBottom)
+
+//to set View's right padding
+fun View.setPaddingRight(value: Int) = setPadding(paddingLeft, paddingTop, value, paddingBottom)
+
+//set an onclick listener
+fun <T : View> T.click(block: (T) -> Unit) = setOnClickListener { block(it as T) }
+
+
+//to set OnClickListener on a view.
+fun <T : View> T.longClick(block: (T) -> Boolean) = setOnLongClickListener { block(it as T) }
+
+//show the view  (visibility = View.VISIBLE)
+fun View.show() : View {
+    if (visibility != View.VISIBLE) {
+        visibility = View.VISIBLE
+    }
+    return this
+}
+
+//hide the view. (visibility = View.INVISIBLE)
+fun View.hide() : View {
+    if (visibility != View.INVISIBLE) {
+        visibility = View.INVISIBLE
+    }
+    return this
+}
+
+
+//remove the view (visibility = View.GONE)
+fun View.remove() : View {
+    if (visibility != View.GONE) {
+        visibility = View.GONE
+    }
+    return this
+}
+
+//toggle a view's visibility
+fun View.toggleVisibility() : View {
+    if (visibility == View.VISIBLE) {
+        visibility = View.INVISIBLE
+    } else {
+        visibility = View.INVISIBLE
+    }
+    return this
+}
+
+
+//to get a view as bitmap.
+fun View.getBitmap(): Bitmap {
+    val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bmp)
+    draw(canvas)
+    canvas.save()
+    return bmp
+}
