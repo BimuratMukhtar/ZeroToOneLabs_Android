b70cc240738a05e857e60e0641d90a72b748f492
lelesy <beis96a@gmail.com>
2018-12-18 14:24:04 +0600

    fix: add package name to settings

--- stepper/src/androidTest/java/com/zerotoonelabs/stepper/ExampleInstrumentedTest.java
+++ stepper/src/androidTest/java/com/zerotoonelabs/stepper/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.zerotoonelabs.mylibrary.test", appContext.getPackageName());
+        assertEquals("com.zerotoonelabs.stepper.test", appContext.getPackageName());
     }
 }
